59215ccc4401bd54f27fac77b5faaa24ce6c61c1
Sivaprakash161999 <sivaprakash161999@gmail.com>
2023-07-29 12:06:58 +0530

    New user authentication and forgot password functionality using mail validations set up

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'irw&@e=@x0m$$#b_$_fupxphc_vo=n2mgmwfq##g^m)0_ru)k6'
+SECRET_KEY = config('SECRET_KEY') # 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True #config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -134,3 +135,17 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP Configuration for Emails
+EMAIL_HOST = config('EMAIL_HOST') # 'smtp.gmail.com'
+EMAIL_PORT = config('EMAIL_PORT', cast=int) # 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') # 'sivaprakash.django.161999@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool) # True
